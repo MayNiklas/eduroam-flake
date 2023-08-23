acd7e2bd274d18a4cf6bf8dcc60574b34242513d
MayNiklas <info@niklas-steffen.de>
2023-08-23 12:21:46 +0200

    init repo

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # eduroam-flake
-install eduroam on Nix systems
+
+Install Eduroam on NixOS.
+
+## Disclaimer
+
+This is technically completely overkill and unnecessary.
+You don't need to create a package to execute a python script when using Nix.
+
+```sh
+nix-shell -p "python3.withPackages (ps: with ps; [ dbus-python ])" --run python3 <your-eduroam.py>
+```
+
+Would technically achieve the same result.
+
+Next month I'm giving a talk about Nix and since we need to login into Eduroam during a live demo, I thought it would be a great and simple example to talk about flakes.
+
+Also: it took me some time to figure out, you need to use `dbus-python` instead of `pydbus`. This repository might save people time in case they find it by googling `NixOS Eduroam`. This repository might be helpful for documentation purposes.
+
+## Usage
+
+> This script assumes you are using NetworkManager.
+
+Find your University's entityID:
+
+```sh
+nix run .#list-eduroam-entityIDs
+```
+
+Create a new entry under `inputs` as well as in the `packages`section of `flake.nix`.
+
+Then run the `nix run` command.
+
+Nice to know:
+To build a Nix package, just run `nix build .#install-eduroam-bonn`.
+The result will be in `result`. You can run it with `./result/bin/install-eduroam-bonn`.
+Reviewing this file manually tells us a lot about how Nix works!
+
+## Supported Universities:
+
+| University       | entityID | command                                      |
+| ---------------- | -------- | -------------------------------------------- |
+| Universität Bonn | 5138     | `nix run .#install-eduroam-university-bonn`  |
+| Universität Köln | 5133     | `nix run .#install-eduroam-university-koeln` |
---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "Install eduroam on NixOS systems";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    # The Eduroam Python script for the University of Bonn
+    eduroam-university-bonn = {
+      url = "https://cat.eduroam.org/user/API.php?action=downloadInstaller&lang=en&profile=5133&device=linux&generatedfor=user&openroaming=0";
+      flake = false;
+    };
+
+    # The Eduroam Python script for the University of Cologne
+    eduroam-university-koeln = {
+      url = "https://cat.eduroam.org/user/API.php?action=downloadInstaller&lang=en&profile=5133&device=linux&generatedfor=user&openroaming=0";
+      flake = false;
+    };
+  };
+
+  outputs = { self, ... }@inputs:
+    with inputs;
+    let
+      supportedSystems = [ "aarch64-linux" "x86_64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; overlays = [ ]; });
+    in
+    {
+      formatter = forAllSystems (system: nixpkgsFor.${system}.nixpkgs-fmt);
+      packages = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+          python-with-dbus = pkgs.python3.withPackages (p: with p; [ dbus-python ]);
+        in
+        {
+          # nix run .#list-eduroam-entityIDs
+          list-eduroam-entityIDs = pkgs.writeShellScriptBin "list-eduroam-entityIDs"
+            "${pkgs.curl}/bin/curl 'https://cat.eduroam.org/user/API.php?action=listAllIdentityProviders&api' | ${pkgs.jq}/bin/jq";
+
+          # nix run .#install-eduroam-university-bonn
+          install-eduroam-university-bonn = pkgs.writeShellScriptBin "install-eduroam-university-bonn"
+            "${python-with-dbus}/bin/python3 ${eduroam-university-bonn}";
+
+          # nix run .#install-eduroam-university-koeln
+          install-eduroam-university-koeln = pkgs.writeShellScriptBin "install-eduroam-university-koeln"
+            "${python-with-dbus}/bin/python3 ${eduroam-university-koeln}";
+        });
+    };
+}
