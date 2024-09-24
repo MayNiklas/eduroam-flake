e922a625ae1bd66826e1c70574cb5e12c505c8fe
MayNiklas <info@niklas-steffen.de>
2024-09-24 19:09:07 +0200

    split into seperate file

--- flake.nix
+++ flake.nix
@@ -10,14 +10,7 @@
       supportedSystems = [ "aarch64-linux" "x86_64-linux" ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
       nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; overlays = [ ]; });
-      unis = [
-        { name = "bonn"; id = 5133; hash = "sha256-uPVriEsQAkjibFm0E2xXtXyD2UVddjPMv2gQfRgKIao="; }
-        { name = "koeln"; id = 5133; hash = "sha256-uPVriEsQAkjibFm0E2xXtXyD2UVddjPMv2gQfRgKIao="; }
-        { name = "leipzig"; id = 5674; hash = "sha256-Clybdol+M5yRYBDxst+14ffThQ/VDYaYeUo6FYyGMpg="; }
-        { name = "lund"; id = 1338; hash = "sha256-8pEpF8YC8VZC76fWio6vtupkYEZTeh/f46pyakbVdF0="; }
-        { name = "saarland"; id = 10315; hash = "sha256-JPR4qeU4TXkaxeDhnXN+DJ8h3Sz5K8b+FNm3UhPoo1I="; }
-        { name = "siegen"; id = 5356; hash = "sha256-fyUno1PytodSyV/fMKqdfAGHZxa3C3m2Nfeskkaotnk="; }
-      ];
+      unis = import ./universities.nix;
     in
     {
       formatter = forAllSystems (system: nixpkgsFor.${system}.nixpkgs-fmt);
---
+++ universities.nix
@@ -0,0 +1,13 @@
+# to add a new university:
+# 1. add a new entry to the list (change the name and id)
+# { name = "new"; id = 1; }
+# 2. run `nix build .\#install-eduroam-new`
+# 3. add the hash from the error to the list entry
+[
+  { name = "bonn"; id = 5133; hash = "sha256-uPVriEsQAkjibFm0E2xXtXyD2UVddjPMv2gQfRgKIao="; }
+  { name = "koeln"; id = 5133; hash = "sha256-uPVriEsQAkjibFm0E2xXtXyD2UVddjPMv2gQfRgKIao="; }
+  { name = "leipzig"; id = 5674; hash = "sha256-Clybdol+M5yRYBDxst+14ffThQ/VDYaYeUo6FYyGMpg="; }
+  { name = "lund"; id = 1338; hash = "sha256-8pEpF8YC8VZC76fWio6vtupkYEZTeh/f46pyakbVdF0="; }
+  { name = "saarland"; id = 10315; hash = "sha256-JPR4qeU4TXkaxeDhnXN+DJ8h3Sz5K8b+FNm3UhPoo1I="; }
+  { name = "siegen"; id = 5356; hash = "sha256-fyUno1PytodSyV/fMKqdfAGHZxa3C3m2Nfeskkaotnk="; }
+]
