4c0cc7cc59f887155a8b1186ac762b73ddfaf642
MayNiklas <info@niklas-steffen.de>
2024-09-24 18:55:39 +0200

    prefetch script

--- flake.nix
+++ flake.nix
@@ -11,12 +11,12 @@
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
       nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; overlays = [ ]; });
       unis = [
-        { name = "bonn"; id = 5133; }
-        { name = "koeln"; id = 5133; }
-        { name = "leipzig"; id = 5674; }
-        { name = "lund"; id = 1338; }
-        { name = "saarland"; id = 10315; }
-        { name = "siegen"; id = 5356; }
+        { name = "bonn"; id = 5133; hash = "sha256-uPVriEsQAkjibFm0E2xXtXyD2UVddjPMv2gQfRgKIao="; }
+        { name = "koeln"; id = 5133; hash = "sha256-uPVriEsQAkjibFm0E2xXtXyD2UVddjPMv2gQfRgKIao="; }
+        { name = "leipzig"; id = 5674; hash = "sha256-Clybdol+M5yRYBDxst+14ffThQ/VDYaYeUo6FYyGMpg="; }
+        { name = "lund"; id = 1338; hash = "sha256-8pEpF8YC8VZC76fWio6vtupkYEZTeh/f46pyakbVdF0="; }
+        { name = "saarland"; id = 10315; hash = "sha256-JPR4qeU4TXkaxeDhnXN+DJ8h3Sz5K8b+FNm3UhPoo1I="; }
+        { name = "siegen"; id = 5356; hash = "sha256-fyUno1PytodSyV/fMKqdfAGHZxa3C3m2Nfeskkaotnk="; }
       ];
     in
     {
@@ -25,9 +25,15 @@
         let
           pkgs = nixpkgsFor.${system};
           python-with-dbus = pkgs.python3.withPackages (p: with p; [ dbus-python ]);
-          mkScript = { name, id, }:
+          mkScript = { name, id, hash ? "", }:
+            let
+              script = pkgs.fetchurl {
+                url = "https://cat.eduroam.org/user/API.php?action=downloadInstaller&lang=en&profile=${builtins.toString id}&device=linux&generatedfor=user&openroaming=0";
+                sha256 = hash;
+              };
+            in
             pkgs.writeShellScriptBin "install-eduroam-${name}" ''
-              ${python-with-dbus}/bin/python <(${pkgs.wget}/bin/wget -qO- "https://cat.eduroam.org/user/API.php?action=downloadInstaller&lang=en&profile=${builtins.toString id}&device=linux&generatedfor=user&openroaming=0")
+              ${python-with-dbus}/bin/python ${script}
             '';
         in
         builtins.listToAttrs
