d2760a882d47d92e6027a4ecac72be88a74a8e67
Aaron Thapa <notsmart98@gmail.com>
2024-09-25 12:07:55 -0400

    add vccs

--- universities.nix
+++ universities.nix
@@ -10,4 +10,5 @@
   { name = "lund"; id = 1338; hash = "sha256-8pEpF8YC8VZC76fWio6vtupkYEZTeh/f46pyakbVdF0="; }
   { name = "saarland"; id = 10315; hash = "sha256-JPR4qeU4TXkaxeDhnXN+DJ8h3Sz5K8b+FNm3UhPoo1I="; }
   { name = "siegen"; id = 5356; hash = "sha256-fyUno1PytodSyV/fMKqdfAGHZxa3C3m2Nfeskkaotnk="; }
+  { name = "vccs"; id = 11835; hash = "sha256-CdO0zwkhBbhkH53yzPMr+EwflZjUTPeOY1XVKHEwIKY="; }
 ]
