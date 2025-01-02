1dd7215d8020632442913140697f89fea32c84be
lakrestofer <lakrestofer@gmail.com>
2025-01-02 13:18:44 +0100

    style: format with nixpkgs-rfc-style

--- flake.nix
+++ flake.nix
@@ -5,25 +5,42 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, ... } @ inputs:
-    with inputs; let
-      supportedSystems = [ "aarch64-linux" "x86_64-linux" ];
+  outputs =
+    { self, ... }@inputs:
+    with inputs;
+    let
+      supportedSystems = [
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; overlays = [ ]; });
+      nixpkgsFor = forAllSystems (
+        system:
+        import nixpkgs {
+          inherit system;
+          overlays = [ ];
+        }
+      );
       unis = import ./universities.nix;
     in
     {
       formatter = forAllSystems (system: nixpkgsFor.${system}.nixpkgs-fmt);
-      packages = forAllSystems (system:
+      packages = forAllSystems (
+        system:
         let
           pkgs = nixpkgsFor.${system};
           python-with-dbus = pkgs.python3.withPackages (p: with p; [ dbus-python ]);
         in
-        builtins.listToAttrs
-          (builtins.map
-            (item: {
-              name = "install-eduroam-${item.name}";
-              value = ({ name, id, hash ? "", }:
+        builtins.listToAttrs (
+          builtins.map (item: {
+            name = "install-eduroam-${item.name}";
+            value =
+              (
+                {
+                  name,
+                  id,
+                  hash ? "",
+                }:
                 let
                   script = pkgs.fetchurl {
                     url = "https://cat.eduroam.org/user/API.php?action=downloadInstaller&lang=en&profile=${builtins.toString id}&device=linux&generatedfor=user&openroaming=0";
@@ -32,14 +49,15 @@
                 in
                 pkgs.writeShellScriptBin "install-eduroam-${name}" ''
                   ${python-with-dbus}/bin/python ${script}
-                '') item;
-            })
-            unis)
-        //
-        {
+                ''
+              )
+                item;
+          }) unis
+        )
+        // {
           # nix run .#list-eduroam-entityIDs
-          list-eduroam-entityIDs = pkgs.writeShellScriptBin "list-eduroam-entityIDs"
-            "${pkgs.curl}/bin/curl 'https://cat.eduroam.org/user/API.php?action=listAllIdentityProviders&api' | ${pkgs.jq}/bin/jq";
-        });
+          list-eduroam-entityIDs = pkgs.writeShellScriptBin "list-eduroam-entityIDs" "${pkgs.curl}/bin/curl 'https://cat.eduroam.org/user/API.php?action=listAllIdentityProviders&api' | ${pkgs.jq}/bin/jq";
+        }
+      );
     };
 }
--- universities.nix
+++ universities.nix
@@ -4,12 +4,44 @@
 # 2. run `nix build .\#install-eduroam-new`
 # 3. add the hash from the error to the list entry
 [
-  { name = "bonn"; id = 5133; hash = "sha256-uPVriEsQAkjibFm0E2xXtXyD2UVddjPMv2gQfRgKIao="; }
-  { name = "flensburg"; id = 5188; hash = "sha256-RRnZbVbFg86GXIOxVChO2e5OoOWu0uzb/3IviECH1GU="; }
-  { name = "koeln"; id = 5133; hash = "sha256-uPVriEsQAkjibFm0E2xXtXyD2UVddjPMv2gQfRgKIao="; }
-  { name = "leipzig"; id = 5674; hash = "sha256-Clybdol+M5yRYBDxst+14ffThQ/VDYaYeUo6FYyGMpg="; }
-  { name = "lund"; id = 1338; hash = "sha256-8pEpF8YC8VZC76fWio6vtupkYEZTeh/f46pyakbVdF0="; }
-  { name = "saarland"; id = 10315; hash = "sha256-JPR4qeU4TXkaxeDhnXN+DJ8h3Sz5K8b+FNm3UhPoo1I="; }
-  { name = "siegen"; id = 5356; hash = "sha256-fyUno1PytodSyV/fMKqdfAGHZxa3C3m2Nfeskkaotnk="; }
-  { name = "vccs"; id = 11835; hash = "sha256-CdO0zwkhBbhkH53yzPMr+EwflZjUTPeOY1XVKHEwIKY="; }
+  {
+    name = "bonn";
+    id = 5133;
+    hash = "sha256-uPVriEsQAkjibFm0E2xXtXyD2UVddjPMv2gQfRgKIao=";
+  }
+  {
+    name = "flensburg";
+    id = 5188;
+    hash = "sha256-RRnZbVbFg86GXIOxVChO2e5OoOWu0uzb/3IviECH1GU=";
+  }
+  {
+    name = "koeln";
+    id = 5133;
+    hash = "sha256-uPVriEsQAkjibFm0E2xXtXyD2UVddjPMv2gQfRgKIao=";
+  }
+  {
+    name = "leipzig";
+    id = 5674;
+    hash = "sha256-Clybdol+M5yRYBDxst+14ffThQ/VDYaYeUo6FYyGMpg=";
+  }
+  {
+    name = "lund";
+    id = 1338;
+    hash = "sha256-8pEpF8YC8VZC76fWio6vtupkYEZTeh/f46pyakbVdF0=";
+  }
+  {
+    name = "saarland";
+    id = 10315;
+    hash = "sha256-JPR4qeU4TXkaxeDhnXN+DJ8h3Sz5K8b+FNm3UhPoo1I=";
+  }
+  {
+    name = "siegen";
+    id = 5356;
+    hash = "sha256-fyUno1PytodSyV/fMKqdfAGHZxa3C3m2Nfeskkaotnk=";
+  }
+  {
+    name = "vccs";
+    id = 11835;
+    hash = "sha256-CdO0zwkhBbhkH53yzPMr+EwflZjUTPeOY1XVKHEwIKY=";
+  }
 ]
